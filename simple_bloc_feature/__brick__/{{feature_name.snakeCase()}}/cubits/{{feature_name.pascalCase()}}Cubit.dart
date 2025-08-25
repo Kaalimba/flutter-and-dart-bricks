c93a8e141701c11469b2ac19d011384c212a962a
Aziz Soulé <aziz.soule.dev@gmail.com>
2025-08-25 13:21:43 +0000

    feat: add package name variable and update cubit structure for improved organization

--- simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}Cubit.dart
+++ simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}Cubit.dart
@@ -1,6 +1,14 @@
 import 'package:bloc/bloc.dart';
-import './{{feature_name.pascalCase()}}State.dart';
+import 'package:{{package_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/{{feature_name.pascalCase()}}State.dart';
+import 'package:{{package_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/repositories/{{feature_name.pascalCase()}}Repository.dart';
 
 class {{feature_name.pascalCase()}}Cubit extends Cubit<{{feature_name.pascalCase()}}State> {
-  {{feature_name.pascalCase()}}Cubit() : super({{feature_name.pascalCase()}}Initial());
+  {{feature_name.pascalCase()}}Cubit({
+    required {{feature_name.pascalCase()}}Repository {{feature_name.pascalCase()}}Repository,
+  }) : _{{feature_name.pascalCase()}}Repository = {{feature_name.pascalCase()}}Repository,
+  super({{feature_name.pascalCase()}}State());
+
+  final {{feature_name.pascalCase()}}Repository _{{feature_name.pascalCase()}}Repository;
+
+  // Add your cubit methods here
 }
---
+++ simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}State.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class {{feature_name.pascalCase()}}State extends Equatable {
+  const LoginState();
+
+  // Add your state's attributes here
+
+  @override
+  List<Object?> get props => [/*Add your state's attributes here too*/];
+}
