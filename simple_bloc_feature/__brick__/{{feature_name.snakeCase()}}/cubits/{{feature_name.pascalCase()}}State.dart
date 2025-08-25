9badaaf11b6a9f63972836d226c0fc7c49c9f2c3
Aziz Soulé <aziz.soule.dev@gmail.com>
2025-08-25 13:30:29 +0000

    fix: correct typo in class name for {{feature_name.pascalCase()}}State

--- simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}State.dart
+++ simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}State.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 class {{feature_name.pascalCase()}}State extends Equatable {
-  const {{eature_name.pascalCase()}}State();
+  const {{feature_name.pascalCase()}}State();
 
   // Add your state's attributes here
 
