5ef58520db98fc0d9be044788876c282f16e77c4
Aziz Soulé <aziz.soule.dev@gmail.com>
2025-08-25 13:28:03 +0000

    fix: update repository variable naming and adjust import paths for better clarity

--- simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}Cubit.dart
+++ simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}Cubit.dart
@@ -1,14 +1,14 @@
 import 'package:bloc/bloc.dart';
-import 'package:{{package_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/{{feature_name.pascalCase()}}State.dart';
+import 'package:{{package_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}State.dart';
 import 'package:{{package_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/repositories/{{feature_name.pascalCase()}}Repository.dart';
 
 class {{feature_name.pascalCase()}}Cubit extends Cubit<{{feature_name.pascalCase()}}State> {
   {{feature_name.pascalCase()}}Cubit({
-    required {{feature_name.pascalCase()}}Repository {{feature_name.pascalCase()}}Repository,
-  }) : _{{feature_name.pascalCase()}}Repository = {{feature_name.pascalCase()}}Repository,
-  super({{feature_name.pascalCase()}}State());
+    required {{feature_name.pascalCase()}}Repository {{feature_name.camelCase()}}Repository,
+  }) : _{{feature_name.camelCase()}}Repository = {{feature_name.pascalCase()}}Repository,
+       super(const {{feature_name.pascalCase()}}State());
 
-  final {{feature_name.pascalCase()}}Repository _{{feature_name.pascalCase()}}Repository;
+  final {{feature_name.pascalCase()}}Repository _{{feature_name.camelCase()}}Repository;
 
   // Add your cubit methods here
 }
