ab7f047f6197fac8061ea041c126bd0726e94504
Aziz Soulé <aziz.soule.dev@gmail.com>
2025-08-25 13:31:52 +0000

    fix: correct variable assignment in {{feature_name.pascalCase()}}Cubit constructor

--- simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}Cubit.dart
+++ simple_bloc_feature/__brick__/{{feature_name.snakeCase()}}/cubits/{{feature_name.pascalCase()}}Cubit.dart
@@ -5,7 +5,7 @@ import 'package:{{package_name.snakeCase()}}/features/{{feature_name.snakeCase()
 class {{feature_name.pascalCase()}}Cubit extends Cubit<{{feature_name.pascalCase()}}State> {
   {{feature_name.pascalCase()}}Cubit({
     required {{feature_name.pascalCase()}}Repository {{feature_name.camelCase()}}Repository,
-  }) : _{{feature_name.camelCase()}}Repository = {{feature_name.pascalCase()}}Repository,
+  }) : _{{feature_name.camelCase()}}Repository = {{feature_name.camelCase()}}Repository,
        super(const {{feature_name.pascalCase()}}State());
 
   final {{feature_name.pascalCase()}}Repository _{{feature_name.camelCase()}}Repository;
